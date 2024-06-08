662f7220efa5bd6a535a431229dbfea7169f9caa
Gabrielle Arrache <gabriellekolzenti@gmail.com>
2024-06-08 16:49:45 -0300

    Aula s02

---
+++ aritmetico.py
@@ -0,0 +1,19 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número:'))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+divisaoInteira = num1 // num2
+modulo = num1 % num2
+potencia = num1 ** num2
+
+print('\n Resultados das operações aritmeticas:')
+print('Soma: ', soma)
+print('Subtracao: ', subtracao)
+print('Multiplicacao: ', multiplicacao)
+print('Divisao: ', divisao)
+print('DivisaoInteira: ', divisaoInteira)
+print('Modulo: ', modulo)
+print('Potencia: ', potencia)
